a0e55c674306057d7a3603ef889ed53069b232b5
AlexanderOkwuchi20 <okwuchialexander@gmail.com>
2023-03-23 13:40:20 +0100

    Team EliteDev

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let container = document.querySelector(".kevin");
+
+window.addEventListener("load",()=>{
+    container.style.display = "none"
+});
+
+
+
+
+let body = document.body
+let toggle = document.querySelector(".toggle");
+let icon = document.querySelector(".fa-moon");
+let iconB = document.querySelector(".dark");
+
+toggle.addEventListener("click",()=>{
+    body.classList.contains("dark")
+    if (body.classList.toggle("dark")){
+        icon.classList.replace("fa-moon", "fa-sun");
+   } else {  icon.classList.replace("fa-moon", "fa-sun");
+    }  {
+        icon.classList.remove("fa-sun")
+        icon.classList.add("fa-moon")
+    }
+});
+
+
+
+
+let icon = document.querySelector(".shower");
+let change = document.querySelector(".show");
+let changeB = document.querySelector(".grab")
+
+change.addEventListener("click", ()=>{
+    change.classList.toggle("shower")
+    if (changeB.type === "password") {
+        changeB.type = "text";
+    } else{
+        changeB.type = "password"
+    };
+});
